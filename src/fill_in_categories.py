0dbcaed0d0843af5bfb28e6df771cc865ba7375a
Pavel Maksimov <maksimov.pavel.an@yandex.ru>
2023-05-21 18:22:20 +0500

    Add module to insert categories into database.

---
+++ src/fill_in_categories.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from budget_bot_2.db_connection import Session
+from budget_bot_2.models import IncomeCategory, OutcomeCategory
+
+INCOME_CATEGORIES = ["зарплата", "другое"]
+
+OUTCOME_CATEGORIES = [
+    "продукты",
+    "общественный траспорт",
+    "аптека",
+    "кафе",
+    "подписки",
+    "развлечения",
+    "бытовые расходы",
+    "мобильная связь",
+    "интернет",
+    "животные",
+    "дать в долг",
+    "коммунальные",
+    "книги",
+    "налоги",
+    "автомобиль",
+]
+
+
+async def fill_in_categories():
+    async with Session() as session:
+        income_categories = [IncomeCategory(name=cat) for cat in INCOME_CATEGORIES]
+        session.add_all(income_categories)
+        outcome_caregories = [OutcomeCategory(name=cat) for cat in OUTCOME_CATEGORIES]
+        session.add_all(outcome_caregories)
+        await session.commit()
+
+
+if __name__ == "__main__":
+    asyncio.run(fill_in_categories())
